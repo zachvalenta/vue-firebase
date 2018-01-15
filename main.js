a9d75c6917ac24d0ae32b5fea0a946f1a78efb17
zachvalenta <zjv5002@gmail.com>
2018-01-15 13:04:03 -0500

    basic persistance for add el, iron out basic formatting

--- main.js
+++ main.js
@@ -6,6 +6,37 @@ var app = new Vue({
 		characters: [],
 		favorite_chars: [],
 	},
+	beforeCreate(){
+    	
+    	// https://stackoverflow.com/a/40714462/6813490
+
+    	var db = firebase.database().ref(); //ref
+    	
+    	// on() is event listener, which makes more since with other args like child_added
+    	// 'value' refers to value of node specified in conf
+    	// 'snap' is name of callback
+    	db.on('value', snap => {
+    		
+    		// 'snap' has other metadata, so need 'val()' to actually get JSON
+    		var favs = snap.val();
+    		
+    		if(favs === null){
+    			console.log("no data");
+    			return;
+    		}
+
+    		/*have: constantly empty container and refill*/
+    		/*want: on DOM load, *one-time*, pull from Firebase*/ 
+    		//https://stackoverflow.com/questions/40714319/how-to-call-a-vue-js-function-on-page-load
+    		// https://vuejs.org/v2/api/#vm-once
+    		this.favorite_chars = [];
+    		var fav_char_names = Object.values(favs);
+    		for (var i = 0; i < fav_char_names.length; i++) {
+    			console.log(fav_char_names[i]);
+    			this.favorite_chars.push(fav_char_names[i]);
+    		}
+    	});
+ 	},
 	methods: {
 		search: function(){
 			if(this.query){
@@ -29,9 +60,10 @@ var app = new Vue({
 			}
 		},
 		add_to_fav: function(li){
-			console.log(li);
-			this.favorite_chars.push(li);
-			console.log(this.favorite_chars)
+			var db = firebase.database().ref();
+			db.push(li, function (err) {
+				if(err){console.log("couldn't push to Firebase");}
+			});
 		},
 		rm_from_fav: function(li){
 			console.log(li);
